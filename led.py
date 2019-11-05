0c85806e86b9ab9cd9d47de21dbda2453e3a0840
Nils <spuravs85@gmail.com>
2019-11-05 10:26:13 +0200

    Update led.py

--- led.py
+++ led.py
@@ -5,16 +5,18 @@ GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
 
 counter = 0
+time.sleep = 1
 try:
     while counter < 30:
         print("Led 1 on")
         GPIO.output(18,GPIO.HIGH)
-        time.sleep(0.2)
+        time.sleep
         print("Led off")
         GPIO.output(18,GPIO.LOW)
-        time.sleep(0.2)
+        time.sleep
         print("Kaut kas jauns")
         counter = counter + 1
+        time.sleep = time.sleep - 0.09
 except KeyboardInterrupt:
         print("/n"), counter
 finally:
