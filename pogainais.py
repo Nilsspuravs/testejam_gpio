3766647d0ef1da9fb4215dea52ee9aaeeaa2ea24
Nils <spuravs85@gmail.com>
2019-11-12 09:18:45 +0200

    Update pogainais.py

--- pogainais.py
+++ pogainais.py
@@ -2,16 +2,27 @@ import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
+GPIO.setup(22,GPIO.OUT)
 
-button = 4
-
+button1 = 4
 GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
 
-while True:
+counter = 100
+laiks = 1.4
+try:
+    while counter < 100:
     button_state = GPIO.input(button)
     if button_state == GPIO.HIGH:
         print ("poga1_HIGH")
-        time.sleep(1)
+        GPIO.output(17,GPIO.HIGH)
+        time.sleep(laiks)
+        counter = counter + 1
+        laiks = laiks - 0.01
+
+        
     else:
         print("poga1_LOW")
-        time.sleep(1)
+        GPIO.output(17,GPIO.LOW)
+        
