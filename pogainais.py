f3957713edc6fe7dc3b18044876fad520f2a0fd1
Nils <spuravs85@gmail.com>
2019-11-12 10:03:48 +0200

    Update pogainais.py

--- pogainais.py
+++ pogainais.py
@@ -5,32 +5,33 @@ GPIO.setwarnings(False)
 GPIO.setup(17,GPIO.OUT)
 GPIO.setup(27,GPIO.OUT)
 GPIO.setup(22,GPIO.OUT)
-
 GPIO.setup(4, GPIO.IN, GPIO.PUD_UP)
-
-counter = 0
-laiks = 1.4
+GPIO.setup(18, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(23, GPIO.IN, GPIO.PUD_UP)
 try:
-    while counter < 100:
-        button_state = GPIO.input(4)
-    if button_state == GPIO.HIGH:
-        print ("poga1_HIGH")
-        time.sleep(0.2)
-        GPIO.output(17,GPIO.LOW)
+    while True:
+        button_state1 = GPIO.input(4)
+        button_state2 = GPIO.input(18)
+        button_state3 = GPIO.input(23)
+    if button_state1 == GPIO.LOW:
+        GPIO.output(17,GPIO.HIGH)
+        GPIO.output(27,GPIO.HIGH)
+        GPIO.output(22,GPIO.HIGH)
+        time.sleep(0.5)
+    
         
 
         
     else :
-        print("poga1_LOW")
-        time.sleep(0.2)
-        GPIO.output(17,GPIO.HIGH)
-        time.sleep(laiks)
         GPIO.output(17,GPIO.LOW)
-        time.sleep(laiks)
-        counter = counter + 1
-        laiks = laiks - 0.01
+        GPIO.output(27,GPIO.LOW)
+        GPIO.output(22,GPIO.LOW)
+        time.sleep(0.5)
+
 except KeyboardInterrupt:
-        print("ble")
+        print("/n"), counter
+finally:
+            GPIO.cleanup()
     
 
         
