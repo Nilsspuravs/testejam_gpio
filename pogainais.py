1f4bc9e2ea4dfa78cbae4c3b443aa112f66bc305
Nils <spuravs85@gmail.com>
2019-11-12 10:11:32 +0200

    Update pogainais.py

--- pogainais.py
+++ pogainais.py
@@ -8,11 +8,17 @@ GPIO.setup(22,GPIO.OUT)
 GPIO.setup(4, GPIO.IN, GPIO.PUD_UP)
 GPIO.setup(18, GPIO.IN, GPIO.PUD_UP)
 GPIO.setup(23, GPIO.IN, GPIO.PUD_UP)
+print("DOne")
 try:
     while True:
         button_state1 = GPIO.input(4)
         button_state2 = GPIO.input(18)
         button_state3 = GPIO.input(23)
+        print("Done1")
+        GPIO.output(17,GPIO.LOW)
+        GPIO.output(27,GPIO.LOW)
+        GPIO.output(22,GPIO.LOW)
+
     if button_state1 == GPIO.LOW:
         GPIO.output(17,GPIO.HIGH)
         GPIO.output(27,GPIO.HIGH)
