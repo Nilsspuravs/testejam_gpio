3b5fe5d539578361878e39950a71b1a6c6e2c269
Nils <spuravs85@gmail.com>
2019-11-19 08:57:57 +0200

    Update pogainais.py

--- pogainais.py
+++ pogainais.py
@@ -9,23 +9,46 @@ GPIO.setup(4, GPIO.IN, GPIO.PUD_UP)
 GPIO.setup(18, GPIO.IN, GPIO.PUD_UP)
 GPIO.setup(23, GPIO.IN, GPIO.PUD_UP)
 print("DOne")
+laiks = 2
+counter = 0
 try:
     while True:
         button_state1 = GPIO.input(4)
         button_state2 = GPIO.input(18)
         button_state3 = GPIO.input(23)
         if button_state1 == GPIO.LOW:
-            print("11111")
+            
             GPIO.output(17,GPIO.HIGH)
             GPIO.output(27,GPIO.HIGH)
             GPIO.output(22,GPIO.HIGH)
-            time.sleep(0.5)
+            time.sleep(0.1)
+        if button_state2 == GPIO.LOW:
+            time.sleep(1)
+            GPIO.output(17,GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(17,GPIO.LOW)
+            GPIO.output(27,GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(27,GPIO.LOW)
+            GPIO.output(22,GPIO.HIGH)
+            time.sleep(1)
+            time.sleep(0.1)
+        if button_state3 == GPIO.LOW:
+            while counter < 100:
+                GPIO.output(17,GPIO.HIGH)
+                GPIO.output(27,GPIO.HIGH)
+                GPIO.output(22,GPIO.HIGH)
+                time.sleep(laiks)
+                laiks = laiks - 0.02
+                counter = counter + 1
+                 
+
         else :
-            print("2222")
+            
             GPIO.output(17,GPIO.LOW)
             GPIO.output(27,GPIO.LOW)
             GPIO.output(22,GPIO.LOW)
-            time.sleep(0.5)
+            time.sleep(0.1)
 
 except KeyboardInterrupt:
         print("/n")
