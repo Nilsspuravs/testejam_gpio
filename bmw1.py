30687c03ebb835dd9b939150070e1e96f97e5b05
Nils <spuravs85@gmail.com>
2019-11-26 10:12:39 +0200

    Update bmw1.py

--- bmw1.py
+++ bmw1.py
@@ -8,7 +8,7 @@ bus = smbus2.SMBus(port)
 calibration_params = bme280.load_calibration_params(bus, address)
 
 data = bme280.sample(bus, address, calibration_params)
-print((datetime.now().strftime('%Y-%m-%d %H:%M:%S'),("I"), (data.temperature),("I"), (data.pressure)), file=open("output.txt", "a+"))
+print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'),(I), (data.temperature),(I), (data.pressure), file=open("output.txt", "a+"))
 print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 print(data.temperature)
 print(data.pressure)
