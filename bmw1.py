f10d39310c14288341361b67cd573a4c7d54b703
Nils <spuravs85@gmail.com>
2019-11-26 10:25:40 +0200

    Update bmw1.py

--- bmw1.py
+++ bmw1.py
@@ -1,6 +1,8 @@
 import smbus2
 import bme280
 from datetime import datetime
+def truncate(n):
+        return int(n * 1000) / 1000
 
 port = 1
 address = 0x76
@@ -8,7 +10,7 @@ bus = smbus2.SMBus(port)
 calibration_params = bme280.load_calibration_params(bus, address)
 I = " "
 data = bme280.sample(bus, address, calibration_params)
-print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'),(I), (data.temperature),(I), (data.pressure), file=open("output.txt", "a+"))
+print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'),(I), truncate(data.temperature, 2),(I), truncate(data.pressure, 2), file=open("output.txt", "a+"))
 print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 print(data.temperature)
 print(data.pressure)
